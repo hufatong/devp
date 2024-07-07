e13ddb193e05d5253a11613f58314e2fbebeee0a
hufatong <hufatong@126.com>
2024-07-07 17:37:20 +0800

    v1.0

---
+++ src/test/java/org/hft/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hft.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
